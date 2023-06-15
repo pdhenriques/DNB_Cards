b050f96b696f40ab5c1f70bfae5056c526d6df6c
Pedro Henriques <pdhenriques@gmail.com>
2023-06-15 20:56:16 +0200

    Added Code for the Lambda function

---
+++ lambda.js
@@ -0,0 +1,71 @@
+const AWS = require('aws-sdk');
+
+const db = new AWS.DynamoDB.DocumentClient();
+const dbTableName = 'CC_Storage_Unit';
+
+exports.handler = async (event, context) => {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+
+    let body;
+    let statusCode = '200';
+
+    try {
+        switch (event.httpMethod) {
+            case 'GET':
+                body = await getCards();
+                break;
+            case 'POST':
+                body = await addCard(JSON.parse(event.body));
+                break;
+            default:
+                throw new Error(`Unsupported method "${event.httpMethod}"`);
+        }
+    } catch (err) {
+        statusCode = '400';
+        body = err.message;
+    }
+
+    return buildResponse(statusCode, body);
+};
+
+async function addCard(requestBody) {
+    const params = {
+        TableName: dbTableName,
+        Item: requestBody
+    }
+    return await db.put(params).promise().then(() => {
+        const body = {
+            Operation: 'Adding Card to DB',
+            Message: 'SUCCESS! Card added to DB.'
+        }
+        return body;
+    }, (error) => {
+        console.error('Error: could not addCard() to DynamoDB: ', error);
+    })
+}
+
+async function getCards() {
+    const params = {
+        TableName: dbTableName,
+    }
+    return await db.scan(params).promise().then((allCards) => {
+        const body = {
+            Operation: 'Getting all Cards from DB',
+            Message: 'SUCCESS! Card added to DB.',
+            Cards: allCards
+        }
+        return body;
+    }, (error) => {
+        console.error('Error: could not addCard() to DynamoDB: ', error);
+    })
+}
+
+function buildResponse(statusCode, body) {
+    return {
+        statusCode: statusCode,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    }
+}
